b37d75b9f455da87a19521d12ef54cf248b73292
Srikar <v.srikar@gmail.com>
2020-06-22 00:25:21 +0530

    Return random temperature.

--- src/main/java/com/srikar/powermock/powermock_demo/TemperatureUtil.java
+++ src/main/java/com/srikar/powermock/powermock_demo/TemperatureUtil.java
@@ -1,11 +1,13 @@
 package com.srikar.powermock.powermock_demo;
 
+import java.util.Random;
+
 public abstract class TemperatureUtil {
 	
 	private TemperatureUtil() {}
 	
-	public static final int getTemperatureOfCityFromWeb(String city) {
+	public static int getTemperatureOfCityFromWeb(String city) {
 		// Invoke a web service to get the real temperature
-		return 24;
+		return new Random().nextInt(100);
 	}
 }
